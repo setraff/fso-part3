a44ee2510c47db66260ff7600692608142b05df7
Rafhaan Fayaz <raf.a.fayaz.unauth@gmail.com>
2023-04-15 21:05:28 +0500

    3.18 done

--- index.js
+++ index.js
@@ -17,28 +17,32 @@ app.get("/api/persons/", (request, response) => {
     .then(res => response.json(res))
 })
 
-app.get("/api/persons/:id", (request, response) => {
-    let {id} = request.params
-    id = Number(id)
-    const person = persons.find(person => person.id === id)
-    if(person)
-    {
-        response.json(person)
-    }
-    else
-    {
-        response.status(404).end()
-    }
+app.get("/api/persons/:id", (request, response, next) => {
+    const {id} = request.params
+    Person.findById(id)
+    .then(x => {
+        if(x)
+        {
+            response.json(x)
+        }
+        else
+        {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-    let {id} = request.params
-    id = Number(id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete("/api/persons/:id", (request, response, next) => {
+    const {id} = request.params
+    Person.findByIdAndRemove(id)
+    .then( _ => {
+        response.status(204).end()
+    })
+    .catch(error => next(error) )
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     let {name, number} = request.body
     if( !(name && number) )
     {
@@ -50,38 +54,51 @@ app.post("/api/persons", (request, response) => {
     const newPerson = new Person({name, number})
     newPerson.save()
     .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
 })
 
 app.get("/info", (request, response) => {
-    response.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date()}</p>
-    `)
+    Person.find({})
+    .then( ({length}) => {
+        response.send(`
+            <p>Phonebook has info for ${length} people</p>
+            <p>${new Date()}</p>
+        `)
+    })
+    .catch(error => next(error))
 })
 
 app.put("/api/persons/:id", (request, response) => {
-    let id = request.params.id
-    id = Number(id)
-    let updated = {}
-    let idInPersons = false
-    persons = persons.map(p => {
-        if(p.id === id)
-        {
-            idInPersons = true
-            updated = {...p, number: request.body.number}
-            return updated
+    const {id} = request.params
+    const {body} = request
+    const {name, number} = body
+    const updatedContact = {name, number}
+    Person.findByIdAndUpdate(id, updatedContact, {new: true})
+    .then( (x) => {
+        if(x) {
+            response.json(x)
         }
-        else
-        {
-            return p
+        else {
+            response.status(404).end()
         }
     })
-    if(!idInPersons)
-    {
-        return response.status(404).end()
-    }
-    response.json(updated)
 })
+
+const handleUnknownRoutes = (request, response) => {
+    response.status(404).send({error: "Route not found"})
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } 
+
+    next(error)
+}
+
+app.use(handleUnknownRoutes, errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {console.log("App running on port 3001")})
